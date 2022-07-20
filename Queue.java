cf93d0c391700c1c4b736457741f0b4e1fd30dfd
meghanai28 <66529359+meghanai28@users.noreply.github.com>
2022-07-19 21:56:16 -0700

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -1,4 +1,5 @@
 
+
 public class Queue extends SinglyLinkedList {
 
 	public Queue()
@@ -6,31 +7,88 @@ public class Queue extends SinglyLinkedList {
 		super();
 	}
 	
-	public void enqueue(Currency val) throws Exception
+	public void enqueue(Currency val) 
 	{
-		addCurrency(val, countCurrency());
+		super.addCurrency(val, super.countCurrency());
 	}
 	
-	public Currency dequeue() throws Exception
+	public Currency dequeue()
 	{
-		Currency val = getStart().getData();
-		removeCurrency(0);
+		Currency val = super.getStart().getData();
+		super.removeCurrency(0);
 		return val;
 	}
 	
-	public Currency peekFront() throws Exception
+	public Currency peekFront() 
 	{
-		return getStart().getData();
+		return super.getStart().getData();
 	}
 	
-	public Currency peekRear() throws Exception
+	public Currency peekRear() 
 	{
-		return getEnd().getData();
+		return super.getEnd().getData();
 	}
 	
-	public void printQueue()
+	public void printQueue() 
 	{
-		printList();
+		super.printList();
+	}
+	
+	
+	
+	
+	
+	public LinkNode getStart() 
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public LinkNode getEnd() 
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public int countCurrency()
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public boolean isListEmpty() 
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public void addCurrency(Currency value, int index) 
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	
+	
+	public void removeCurrency(Currency value)
+	{
+		throw new UnsupportedOperationException();		
+	}
+	
+	
+	public void removeCurrency(int index) 
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public int findCurrency(Currency value) 
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public LinkNode getCurrency(int index) 
+	{
+		throw new UnsupportedOperationException();
+	}
+	
+	public void printList() 
+	{
+		throw new UnsupportedOperationException();
 	}
 	
 }
