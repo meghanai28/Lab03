facaa888f50a665e9d73b3e6442377c2155925d2
meghanai28 <66529359+meghanai28@users.noreply.github.com>
2022-07-21 00:19:54 -0700

    Update SinglyLinkedList.java

--- SinglyLinkedList.java
+++ SinglyLinkedList.java
@@ -1,338 +1,128 @@
 /**
  * Lab 03
  * @author Lab Group 7: Meghana Indukuri & Joseph Khamisy
- * This program creates the class SinglyLinkedList with private attributes and methods that include adding and 
- * deleting nodes with given indexes and Currency values.
+ * This is the program where all of our operations for LinkedList, Stack and Queue are
+ * tested in the main method.
  * July 20th 2022
  */
+public class Main {
+	public static void main (String[] args) throws Exception
+	{
+		Currency []arr = {new Dollar(57.12),new Dollar(23.44), new Dollar(87.43), new Dollar(68.99), new Dollar(111.22),
+		new Dollar(44.55), new Dollar(77.77), new Dollar(18.63), new Dollar(543.21),new Dollar(20.21),
+		new Dollar(345.67),new Dollar(36.18), new Dollar(48.48),new Dollar(101.00),new Dollar(11.00),
+		new Dollar(21.00), new Dollar(51.00), new Dollar(1.00), new Dollar(251.00), new Dollar(151.00)};
+		
+		System.out.println("\nWelcome to SinglyLinkedList, Stack and Queue tester. Created by Meghana Indukuri & Joseph Khamisy  \n\n");
+		
+		System.out.println("Testing SinglyLinkedList....");
+		
+		SinglyLinkedList test = new SinglyLinkedList();
+		int add = 7;
+		for(int i = 0; i<add; i++)
+		{
+			test.addCurrency(arr[i], 0);
+		}
+		
 
-import java.nio.BufferOverflowException;
-import java.nio.BufferUnderflowException;
-import java.util.NoSuchElementException;
+		System.out.println();
+		System.out.println("Index of $87.43:  " + test.findCurrency(new Dollar(87.43)));
+		System.out.println("Index of $44.56:  "+ test.findCurrency(new Dollar(44.56)));
+		
+		test.removeCurrency(new Dollar(111.22));
+	
+		test.removeCurrency(2);
+		
+		System.out.println();
+		System.out.println(	test.printList());
+		System.out.println();
+		
+		add++;
+		for(int i =0; i<4; i++)
+		{
+			test.addCurrency(arr[add], add%5 );
 
-public class SinglyLinkedList {
-	
-	private int count;// number of nodes in list
-	private LinkNode start; // head node
-	private LinkNode end; // tail node
-	
-	/**
-	 * Default constructor, sets both start and end nodes to null
-	 * and sets the count to 0.
-	 */
-	public SinglyLinkedList()
-	{
-		start = null;
-		end = null;
-		count = 0;
-	}
-	
-	/**
-	 * Getter method for starting node
-	 * @pre the linked list must not be empty
-	 * @post throws a noSuchElementException whenever the singlyLinkedList is empty.
-	 * @return the start Linknode.
-	 */
-	public LinkNode getStart() 
-	{
-		if(start == null)
-		{
-			throw new NoSuchElementException();
-		}
-		return start;
-	}
-	
-	/**
-	 * Getter method for ending node
-	 * @pre the linked list must not be empty
-	 * @post throws a noSuchElementException whenever the singlyLinkedList is empty.
-	 * @return the end Linknode.
-	 */
-	public LinkNode getEnd() 
-	{
-		if(end == null)
-		{
-			throw new NoSuchElementException();
-		}
-		return end;
-	}
-	
-	/**
-	 * Returns the count value (i.e the number of nodes)
-	 * @pre
-	 * @post
-	 * @return returns the private attribute int count.
-	 */
-	public int countCurrency() 
-	{
-		return count;
-	}
-	
-	/**
-	 * Checks if the list is empty and returns true or false based on if its empty
-	 * @pre
-	 * @post
-	 * @return if the list is empty, the method returns true, false otherwise.
-	 */
-	public boolean isListEmpty()
-	{
-		return start == null;
-	}
-	
-	
-	/**
-	 * adds a LinkNode to the singlyLinkedList based on a given Currency value and a given index for placement of
-	 * the node. 
-	 * @pre Currency value - the value of the node to be added. value should not be null
-	 * 		int index - the index of where the new node should be added. Index should be in bounds.
-	 * 
-	 * @post if any of the parameters are null or out of bounds an exception is thrown. If there is no more memory
-	 * an overflow exception is thrown. Otherwise a node is added to the singlyLinkedList if all conditions are
-	 * met and then count is incremented.
-	 */
-	public void addCurrency(Currency value, int index) 
-	{
-		try
-		{
-			if(value == null || index<0 || index> count)
-			{
-				throw new IllegalArgumentException();
-			}
-		
-			LinkNode added = new LinkNode(value);
-			if(start == null && index == 0)
-			{
-				start = added;
-				end = added;
-			}
-			else if(start!= null && index ==0)
-			{
-				added.setNext(start);
-				start = added;
-			}
-			else if(index == count)
-			{
-				end.setNext(added);
-				end = added;
-			}
-			else
-			{
-				LinkNode prev = null;
-				LinkNode curr = start;
-				for(int i =0; i<index; i++)
-				{
-					prev = curr;
-					curr = curr.getNext();
-				}
-				added.setNext(curr);
-				prev.setNext(added);
-			}
-			count++;
-		}
-		catch(OutOfMemoryError E)
-		{
-			throw new BufferOverflowException();
+			add++;
 		}
 		
-	}
 	
+		
+		test.removeCurrency(test.countCurrency() % 6);
 	
-	/**
-	 * Removes the first Node that has the same data value as the given currency value.
-	 * @pre Currency value - the value to be searched for and deleted. Cannot be null
-	 *  
-	 * @post  if the parameter is null, a exception is thrown. If the list is empty and the remove method is
-	 * called, an exception is also thrown. Otherwise the currency is searched for and removed.count is decremented.
-	 * However if the method cannot find a matching node to the given currency value, nothing is deleted. 
-	 * @return Currency the removed node's value, returns null if value
-	 * does not exist in linkedList
-	 */
-	public Currency removeCurrency(Currency value) throws Exception 
-	{
-		if(value == null)
+		test.removeCurrency(test.countCurrency() / 7);
+		System.out.println(	test.printList());
+		System.out.println("\nDone Testing Singly Linked List!\n");
+		
+		
+		System.out.println("\n\nTesting Stack....\n");
+		Stack test2 = new Stack();
+		
+		for(int i = 0 ; i<7; i++)
 		{
-			throw new IllegalArgumentException();
-		}
-		if( start == null)
-		{
-			throw new BufferUnderflowException();
+			test2.push(arr[13+i]);
 		}
 		
-		LinkNode prev = null;
-		LinkNode curr = start;
+		System.out.println("Peek at stack: " + test2.peek());
+		test2.pop();
+		test2.pop();
+		test2.pop();
 		
-		while(!curr.getData().isEqual(value) && curr.getNext()!= null)
-		{
-			prev = curr;
-			curr = curr.getNext();
-		}
+		System.out.println();
+		System.out.println(	test2.printStack());
 		
-		if(curr.getData().isEqual(value))
+		for(int i = 0 ; i<5; i++)
 		{
-			if(curr == start)
-			{
-				start = curr.getNext();
-				if(count == 1)
-				{
-					end = null;
-				}
-			}
-			else
-			{
-				if(curr== end)
-				{
-					end = prev;
-				}
-				prev.setNext(curr.getNext());
-				
-			}
-			count--;
-			return curr.getData();
-		}
-		return null;
-	}
-	
-	
-	
-	/**
-	 * Removes the node in the singlyLinkedList at the given index and returns the node's currency value.
-	 * @pre int index - the index where the node should be deleted. Cannot be an outbounds/invalid index.
-	 * 
-	 * @post if the list is empty, and the remove method is called, there is an underflow exception thrown.
-	 * Otherwise, the node at the index is removed and the count is decremented.
-	 * @return Currency the removed node's value
-	 */
-	public Currency removeCurrency(int index) 
-	{
-		if( start == null )
-		{
-			throw new BufferUnderflowException();
-		}
-		
-		if(index<0 || index>=count)
-		{
-			throw new IllegalArgumentException();
+			test2.push(arr[i]);
 		}
 		
 		
-		LinkNode prev = null;
-		LinkNode curr = start;
+		test2.pop();
+		test2.pop();
+		
+		System.out.println();
+		System.out.println(test2.printStack());
+		
+		System.out.println("\nDone Testing Stack!\n");
+		
+		
+		System.out.println("\n\nTesting Queue....\n");
+		Queue test3 = new Queue();
+		for(int i = 0 ; i<7; i++)
+		{
+			test3.enqueue(arr[5+i]);
+		}
+		
+		System.out.println("Peek at front of queue: " + test3.peekFront());
+		System.out.println();
+		System.out.println("Peek at end of queue: " + test3.peekRear());
 
-		if(index ==0)
-		{	
-			start = curr.getNext();	
-			if(count == 1)
-			{
-				end = null;
-			}	
-		}
-		else
+		test3.dequeue();
+		test3.dequeue();
+		
+		System.out.println();
+		System.out.println(test3.printQueue());
+		System.out.println();
+		
+		for(int i = 0 ; i<5; i++)
 		{
-			
-			for(int i =0; i<index; i++)
-			{
-				prev = curr;
-				curr = curr.getNext();		
-			}
-			
-			if(end == curr)
-			{
-				end = prev;
-			}
-			
-			prev.setNext(curr.getNext());
+			test3.enqueue(arr[10+i]);
 		}
-		count--;
-		return curr.getData();
+		
+		test3.dequeue();
+		test3.dequeue();
+		test3.dequeue();
+		
+		
+		System.out.println(test3.printQueue());
+		System.out.println("\nDone Testing Queue!");
+		
+		//cleaning up 
+		arr = null;
+		test = null;
+		test2 = null;
+		test3 = null;
+		
+		System.out.println("\n\nWe have compeleted all the tests! Thank you for using this program.");
 		
 	}
-	
-	
-	/**
-	 * Finds the index of given Currency Value. If no node matches the given currency value
-	 * -1 is returned. 
-	 * @pre Given currency value to be searched for. Value cannot be null
-	 * @post if the given parameter is null, an exception is thrown. Or if the linkedList is empty exception is thrown.
-	 * @return int, the index of the given currency value. If no node matches given
-	 * value then -1 is returned to signify that. 
-	 */
-	public int findCurrency(Currency value) throws Exception 
-	{
-		if( start == null)
-		{
-			throw new IllegalStateException();
-		}
-		
-		if(value == null)
-		{
-			throw new IllegalArgumentException();
-		}
-
-		LinkNode temp = start;
-		int index =0;
-		while(temp!=null && !temp.getData().isEqual(value))
-		{
-			temp = temp.getNext();
-			index++;
-		}
-		if(temp == null)
-		{
-			return -1;
-		}
-		return index;
-	}
-	
-	
-	/**
-	 * Gets the given node at a given index. if the list is empty, the node returned is null.
-	 * @pre int index - the index at which the node should be retrieved. index cannot be outofbounds/invalid.
-	 * @post throws exception when the index is outofbounds/invalid, or if the linkedlist is empty.
-	 * @return Currency at given index. if list empty, node returned is null.
-	 */
-	public Currency getCurrency(int index) 
-	{
-		if( start == null)
-		{
-			throw new IllegalStateException();
-		}
-		
-		if(index<0 || index>= count)
-		{
-			throw new IllegalArgumentException();
-		}
-		
-		LinkNode temp = start;
-		for(int i =0; i<index; i++)
-		{
-			temp = temp.getNext();
-		}
-		
-		return temp.getData();
-	}
-	
-	
-	/**
-	 * Returns a string with each Node value seperated by a tab in order of the index.
-	 * @pre
-	 * @post
-	 * @return a string with each Node value seperated by a tab in order of the index.
-	 */
-	public String printList() 
-	{
-		LinkNode temp = start;
-		String val = "";
-		while(temp != null)
-		{
-			val += temp.getData()+ "	";
-			temp = temp.getNext();
-		}
-		
-		return val;
-	}
-	
-
-	
-	
-	
-	
 }
