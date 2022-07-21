f63605ca3fdbb5fc3deb7f44b654e9464fd65333
meghanai28 <66529359+meghanai28@users.noreply.github.com>
2022-07-20 22:03:41 -0700

    Update Queue.java

--- Queue.java
+++ Queue.java
@@ -9,16 +9,33 @@
  */
 public class Queue extends SinglyLinkedList {
 
+	/**
+	 * Default Constructor which calls the super constructor
+	 * to initalize a new Queue derived from the singlyLinkedList
+	 */
 	public Queue()
 	{
 		super();
 	}
 	
+	/**
+	 * Takes a Currency object as parameter and adds it to the end of the queue.
+	 * @pre Currency val must not be null or exception is thrown from
+	 * 		super addCurrency method.
+	 * @post Exception is thrown when val is null or when the Queue is full
+	 */
 	public void enqueue(Currency val) 
 	{
 		super.addCurrency(val, super.countCurrency());
 	}
 	
+	/**
+	 * Removes and returns the Currency object from the front of the queue.
+	 * @pre 
+	 * @post Exception is thrown when queue is empty
+	 * and the method dequeue is called.
+	 * @return Currency val - the val that was dequeued off the queue
+	 */
 	public Currency dequeue()
 	{
 		Currency val = super.getStart().getData();
@@ -26,16 +43,40 @@ public class Queue extends SinglyLinkedList {
 		return val;
 	}
 	
+	/**
+	 * Returns the value at the front of the queue without
+	 * modifying the queue in any way.
+	 * @pre
+	 * @post Throws an exception when the queue is empty and
+	 * peekFront method is called.
+	 * @return the Currency value that was at the front of the queue.
+	 */
 	public Currency peekFront() 
 	{
 		return super.getStart().getData();
 	}
 	
+	/**
+	 * Returns the value at the end of the queue without
+	 * modifying the queue in any way.
+	 * @pre
+	 * @post Throws an exception when the queue is empty and
+	 * peekRear method is called.
+	 * @return the Currency value that was at the end of the queue.
+	 */
 	public Currency peekRear() 
 	{
 		return super.getEnd().getData();
 	}
 	
+	/**
+	 * Returns a string signifying the contents of the 
+	 * queue from the front to end, spaced by a tab.(FIFO)
+	 * @pre
+	 * @post
+	 * @return The string with the queue nodes from front 
+	 * to end.
+	 */
 	public String printQueue() 
 	{
 		return super.printList();
@@ -45,54 +86,135 @@ public class Queue extends SinglyLinkedList {
 	
 	
 	
-	
-	public LinkNode getStart() 
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
+	public LinkNode getStart()
 	{
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
 	public LinkNode getEnd() 
 	{
 		throw new UnsupportedOperationException();
 	}
 	
-	public int countCurrency()
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
+	public int countCurrency() 
 	{
 		throw new UnsupportedOperationException();
 	}
 	
-	public boolean isListEmpty() 
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
+	public boolean isListEmpty()
 	{
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
 	public void addCurrency(Currency value, int index) 
 	{
 		throw new UnsupportedOperationException();
+		
+		
 	}
 	
-	
-	public void removeCurrency(Currency value)
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
+	public void removeCurrency(Currency value) 
 	{
-		throw new UnsupportedOperationException();		
+		throw new UnsupportedOperationException();
+					
 	}
 	
-	
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
 	public void removeCurrency(int index) 
 	{
 		throw new UnsupportedOperationException();
+		
 	}
 	
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
 	public int findCurrency(Currency value) 
 	{
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
 	public LinkNode getCurrency(int index) 
 	{
 		throw new UnsupportedOperationException();
 	}
 	
+	/**
+	 * @override
+	 * This method throws an unsupportedOperationException
+	 * whenever it is called on a stack object.
+	 * @pre
+	 * @post throws an exception
+	 * 
+	 */
 	public String printList() 
 	{
 		throw new UnsupportedOperationException();
